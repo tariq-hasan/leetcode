3810729bfd81f5259371efb55aa64dd39b52398c
tariq-hasan <mmtariquehsn@gmail.com>
2024-03-25 22:38:08 -0400

    Refactored previous solutions

--- two_pointers/easy_1_two_sum.py
+++ two_pointers/easy_1_two_sum.py
@@ -1,11 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        The time complexity is O(n).
+        The space complexity is O(n).
+        """
         map_index = {}
         for i in range(len(nums)):
             complement = target - nums[i]
             if complement in map_index:
                 return [i, map_index[complement]]
             map_index[nums[i]] = i
-
-# The time complexity is O(n).
-# The space complexity is O(n).
--- two_pointers/medium_167_two_sum_ii_-_input_array_is_sorted.py
+++ two_pointers/medium_167_two_sum_ii_-_input_array_is_sorted.py
@@ -1,5 +1,9 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        """
+        The time complexity is O(n).
+        The space complexity is O(1).
+        """
         i, j = 0, len(numbers) - 1
         while i < j:
             total = numbers[i] + numbers[j]
@@ -9,6 +13,3 @@ class Solution:
                 j = j - 1
             else:
                 return [i + 1, j + 1]
-
-# The time complexity is O(n).
-# The space complexity is O(1).
