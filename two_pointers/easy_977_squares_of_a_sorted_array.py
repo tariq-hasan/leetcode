3478940da601935ceba844d95165f02a7d2f3b83
tariq-hasan <mmtariquehsn@gmail.com>
2024-03-30 18:14:53 -0400

    Added a second solution for the squares of a sorted array

--- two_pointers/easy_977_squares_of_a_sorted_array.py
+++ two_pointers/easy_977_squares_of_a_sorted_array.py
@@ -14,3 +14,21 @@ class Solution:
                 res.append(nums[j] * nums[j])
                 j = j - 1
         return res[::-1]
+
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        """
+        The time complexity is O(n).
+        The space complexity is O(1).
+        """
+        res = [0] * len(nums)
+        i, j = 0, len(nums) - 1
+        for k in range(len(nums) - 1, -1, -1):
+            if abs(nums[i]) >= abs(nums[j]):
+                res[k] = nums[i] * nums[i]
+                i = i + 1
+            else:
+                res[k] = nums[j] * nums[j]
+                j = j - 1
+        return res
