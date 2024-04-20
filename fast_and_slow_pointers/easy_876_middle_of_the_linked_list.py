b92128efd4d838c1a2f89d23a4481bf3bbad67d3
tariq-hasan <mmtariquehsn@gmail.com>
2024-04-20 18:59:53 -0400

    Added solution for middle of the linked list

---
+++ fast_and_slow_pointers/easy_876_middle_of_the_linked_list.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        The time complexity is O(n).
+        The space complexity is O(1).
+        """
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
