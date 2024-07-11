8869dc57c5ec3d205c21db423a856f4efbc1a8e2
tariq-hasan <mmtariquehsn@gmail.com>
2024-07-10 22:49:10 -0400

    Added second solution for generate parenthesis

--- coding_patterns/subsets/medium_22_generate_parenthesis.py
+++ coding_patterns/subsets/medium_22_generate_parenthesis.py
@@ -24,3 +24,21 @@ class Solution:
         combinations = []
         backtrack(0, 0, [])
         return combinations
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        """
+        The time complexity is n * C_n = O(4^n / sqrt(n)), where C_n is the Catalan number.
+        The space complexity is O(n).
+        """
+        if n == 0:
+            return ['']
+
+        combinations = []
+        for left_count in range(n):
+            for left_string in self.generateParenthesis(left_count):
+                for right_string in self.generateParenthesis(n - 1 - left_count):
+                    combinations.append('(' + left_string + ')' + right_string)
+
+        return combinations
