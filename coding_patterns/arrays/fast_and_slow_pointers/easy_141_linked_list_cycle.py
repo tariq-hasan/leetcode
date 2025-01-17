3506f1dd7ce6b398efa8545f3b98548dc1c4063e
tariq-hasan <mmtariquehsn@gmail.com>
2025-01-16 23:32:28 -0500

    Added a second solution for linked list cycle

--- coding_patterns/arrays/fast_and_slow_pointers/easy_141_linked_list_cycle.py
+++ coding_patterns/arrays/fast_and_slow_pointers/easy_141_linked_list_cycle.py
@@ -1,10 +1,28 @@
 from typing import Optional
 
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        """
+        The time complexity is O(n).
+        The space complexity is O(n).
+        """
+        seen = set()
+        current = head
+        while current is not None:
+            if current in seen:
+                return True
+            seen.add(current)
+            current = current.next
+        return False
+
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
