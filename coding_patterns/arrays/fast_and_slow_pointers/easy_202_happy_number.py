7e993c0af8820a1509cb9e77cd6e1dd5e9a9c863
tariq-hasan <mmtariquehsn@gmail.com>
2025-01-18 11:02:07 -0500

    Update solution for happy numbers

--- coding_patterns/arrays/fast_and_slow_pointers/easy_202_happy_number.py
+++ coding_patterns/arrays/fast_and_slow_pointers/easy_202_happy_number.py
@@ -4,12 +4,17 @@ class Solution:
         The time complexity is O(log n).
         The space complexity is O(log n).
         """
-        seen = set()
-        while n != 1 and n not in seen:
-            seen.add(n)
+        def get_next(n):
             total = 0
             while n:
                 total = total + ((n % 10) * (n % 10))
                 n = n // 10
-            n = total
+            return total
+
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = get_next(n)
         return n == 1
+
+
