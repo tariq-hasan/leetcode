cee2ff1aa09424a64940598d48fee1db5c44dd79
tariq-hasan <mmtariquehsn@gmail.com>
2025-01-18 11:04:55 -0500

    Add second solution for happy numbers

--- coding_patterns/arrays/fast_and_slow_pointers/easy_202_happy_number.py
+++ coding_patterns/arrays/fast_and_slow_pointers/easy_202_happy_number.py
@@ -18,3 +18,21 @@ class Solution:
         return n == 1
 
 
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        """
+        The time complexity is O(log n).
+        The space complexity is O(1).
+        """
+        def get_next(n):
+            total = 0
+            while n:
+                total = total + ((n % 10) * (n % 10))
+                n = n // 10
+            return total
+
+        slow_runner, fast_runner = n, get_next(n)
+        while fast_runner != 1 and slow_runner != fast_runner:
+            slow_runner = get_next(slow_runner)
+            fast_runner = get_next(get_next(fast_runner))
+        return fast_runner == 1
