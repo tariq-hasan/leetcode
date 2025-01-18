abdf77b9e7962d1ab6ccb59f7c52e63936141dde
tariq-hasan <mmtariquehsn@gmail.com>
2025-01-17 22:41:59 -0500

    Add a second solution for longest substring with at least k repeating characters

--- coding_patterns/arrays/sliding_window/medium_395_longest_substring_with_at_least_k_repeating_characters.py
+++ coding_patterns/arrays/sliding_window/medium_395_longest_substring_with_at_least_k_repeating_characters.py
@@ -22,3 +22,42 @@ class Solution:
             return end - start
 
         return longest_substring_util(s, 0, len(s), k)
+
+
+class Solution:
+    def longestSubstring(self, s: str, k: int) -> int:
+        """
+        The time complexity is O(n).
+        The space complexity is O(1).
+        """
+        unique = [False] * 26
+        max_unique = 0
+        for i in range(len(s)):
+            if not unique[ord(s[i]) - ord('a')]:
+                max_unique = max_unique + 1
+                unique[ord(s[i]) - ord('a')] = True
+
+        out = 0
+        for curr_unique in range(1, max_unique + 1):
+            freq = [0] * 26
+            i = j = unique = count_at_least_k = 0
+            while j < len(s):
+                if unique <= curr_unique:
+                    idx = ord(s[j]) - ord('a')
+                    if freq[idx] == 0:
+                        unique = unique + 1
+                    freq[idx] = freq[idx] + 1
+                    if freq[idx] == k:
+                        count_at_least_k = count_at_least_k + 1
+                    j = j + 1
+                else:
+                    idx = ord(s[i]) - ord('a')
+                    if freq[idx] == k:
+                        count_at_least_k = count_at_least_k - 1
+                    freq[idx] = freq[idx] - 1
+                    if freq[idx] == 0:
+                        unique = unique - 1
+                    i = i + 1
+                if unique == curr_unique and unique == count_at_least_k:
+                    out = max(out, j - i)
+        return out
