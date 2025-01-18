533affe69d5ab089ba37cf390bf6e44532026052
tariq-hasan <mmtariquehsn@gmail.com>
2025-01-18 13:03:45 -0500

    Add second solution for palindrome linked list

--- coding_patterns/arrays/fast_and_slow_pointers/easy_234_palindrome_linked_list.py
+++ coding_patterns/arrays/fast_and_slow_pointers/easy_234_palindrome_linked_list.py
@@ -23,3 +23,37 @@ class Solution:
             return True
 
         return check_palindrome(head)
+
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        """
+        The time complexity is O(n).
+        The space complexity is O(1).
+        """
+        def end_of_first_half(head: ListNode) -> ListNode:
+            slow = fast = head
+            while fast.next and fast.next.next:
+                slow = slow.next
+                fast = fast.next.next
+            return slow
+
+        def reverse_list(head: ListNode) -> ListNode:
+            prev, curr = None, head
+            while curr:
+                next_node, curr.next = curr.next, prev
+                prev, curr = curr, next_node
+            return prev
+
+        first_half_end = end_of_first_half(head)
+        second_half_start = reverse_list(first_half_end.next)
+
+        result = True
+        first, second = head, second_half_start
+        while result and second:
+            if first.val != second.val:
+                result = False
+            first, second = first.next, second.next
+
+        first_half_end.next = reverse_list(second_half_start)
+        return result
