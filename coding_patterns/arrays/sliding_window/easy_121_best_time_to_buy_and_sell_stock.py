9db829a439ac2cb7d1fb46d8ff9ab6b636bdf5a4
tariq-hasan <mmtariquehsn@gmail.com>
2025-01-27 22:28:35 -0500

    Update solution for best time to buy and sell stock

--- coding_patterns/arrays/sliding_window/easy_121_best_time_to_buy_and_sell_stock.py
+++ coding_patterns/arrays/sliding_window/easy_121_best_time_to_buy_and_sell_stock.py
@@ -6,11 +6,9 @@ class Solution:
         The time complexity is O(n).
         The space complexity is O(1).
         """
-        min_price, max_profit = float('inf'), 0
-        for i in range(len(prices)):
-            profit = prices[i] - min_price
-            if profit > max_profit:
-                max_profit = profit
-            elif prices[i] < min_price:
-                min_price = prices[i]
-        return max_profit
+        left = out = 0
+        for i in range(1, len(prices)):
+            out = max(out, prices[i] - prices[left])
+            if prices[i] < prices[left]:
+                left = i
+        return out
