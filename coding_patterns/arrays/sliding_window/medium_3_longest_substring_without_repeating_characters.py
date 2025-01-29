13695c3e91cfe9dc2501b5ad7d56586c8b9bc20b
tariq-hasan <mmtariquehsn@gmail.com>
2025-01-28 21:09:49 -0500

    Add second solution for longest substring without repeating characters

--- coding_patterns/arrays/sliding_window/medium_3_longest_substring_without_repeating_characters.py
+++ coding_patterns/arrays/sliding_window/medium_3_longest_substring_without_repeating_characters.py
@@ -16,3 +16,27 @@ class Solution:
         return list(out)
 
 
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        """
+        The time complexity is O(n).
+        The space complexity is O(n).
+        """
+        if len(s) <= 10:
+            return []
+
+        to_int = {"A": 0, "C": 1, "G": 2, "T": 3}
+        nums = [to_int.get(s[i]) for i in range(len(s))]
+
+        h = 0
+        seen, out = set(), set()
+        for i in range(len(s) - 9):
+            if i != 0:
+                h = (h * 4) - (nums[i - 1] * (4 ** 10)) + nums[i + 9]
+            else:
+                for i in range(10):
+                    h = (h * 4) + nums[i]
+            if h in seen:
+                out.add(s[i : i + 10])
+            seen.add(h)
+        return list(out)
