e6e84292a7ccc4486ac6ed2dbeb16884d8697093
tariq-hasan <mmtariquehsn@gmail.com>
2025-01-28 22:29:07 -0500

    Add second solution for longest substring without repeating characters

--- coding_patterns/arrays/sliding_window/medium_3_longest_substring_without_repeating_characters.py
+++ coding_patterns/arrays/sliding_window/medium_3_longest_substring_without_repeating_characters.py
@@ -14,3 +14,20 @@ class Solution:
             seen.add(s[j])
             out = max(out, len(seen))
         return out
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        The time complexity is O(n).
+        The space complexity is O(n).
+        """
+        freq = {}
+        i = out = 0
+        for j in range(len(s)):
+            freq[s[j]] = freq.get(s[j], 0) + 1
+            while freq[s[j]] > 1:
+                freq[s[i]] = freq[s[i]] - 1
+                i = i + 1
+            out = max(out, j - i + 1)
+        return out
