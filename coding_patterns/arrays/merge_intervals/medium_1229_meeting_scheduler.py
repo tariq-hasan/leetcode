0fc9242724f273f5aba0d8bb5a0437d22d4b5c71
tariq-hasan <mmtariquehsn@gmail.com>
2025-02-19 00:23:53 -0500

    Add second solution for meeting scheduler

--- coding_patterns/arrays/merge_intervals/medium_1229_meeting_scheduler.py
+++ coding_patterns/arrays/merge_intervals/medium_1229_meeting_scheduler.py
@@ -19,3 +19,20 @@ class Solution:
             else:
                 j = j + 1
         return []
+
+
+class Solution:
+    def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
+        """
+        The time complexity is O(n log n).
+        The space complexity is O(n).
+        """
+        timeslots = list(filter(lambda x: x[1] - x[0] >= duration, slots1 + slots2))
+        heapq.heapify(timeslots)
+
+        while len(timeslots) > 1:
+            start1, end1 = heapq.heappop(timeslots)
+            start2, end2 = timeslots[0]
+            if end1 >= start2 + duration:
+                return [start2, start2 + duration]
+        return []
