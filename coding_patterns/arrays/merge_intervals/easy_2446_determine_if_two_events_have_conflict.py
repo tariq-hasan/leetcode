fd3e1ffebccaef246473c4d7b0df3edd24e86e91
tariq-hasan <mmtariquehsn@gmail.com>
2025-02-19 22:36:21 -0500

    Update solution for determine if two events have conflict

--- coding_patterns/arrays/merge_intervals/easy_2446_determine_if_two_events_have_conflict.py
+++ coding_patterns/arrays/merge_intervals/easy_2446_determine_if_two_events_have_conflict.py
@@ -1,4 +1,3 @@
-
 from typing import List
 
 
@@ -8,10 +7,10 @@ class Solution:
         The time complexity is O(1).
         The space complexity is O(1).
         """
-        if event2[0] <= event1[0] <= event2[1] or event2[0] <= event1[1] <= event2[1]:
+        if event1[0] <= event2[0] <= event1[1]:
             return True
 
-        if event1[0] <= event2[0] <= event1[1] or event1[0] <= event2[1] <= event1[1]:
+        if event2[0] <= event1[0] <= event2[1]:
             return True
 
         return False
