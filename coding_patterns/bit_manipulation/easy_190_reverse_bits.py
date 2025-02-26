234ce6f4c4ea9ecfe4166706147c3b4252d6491d
tariq-hasan <mmtariquehsn@gmail.com>
2025-02-25 22:48:33 -0500

    Add solution for reverse bits

--- coding_patterns/bit_manipulation/easy_190_reverse_bits.py
+++ coding_patterns/bit_manipulation/easy_190_reverse_bits.py
@@ -10,3 +10,16 @@ class Solution:
             n = n >> 1
             power -= 1
         return res
+
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        """
+        The time complexity if O(1).
+        The space complexity if O(1).        
+        """
+        out = 0
+        for _ in range(32):
+            out = (out << 1) | (n & 1)
+            n = n >> 1
+        return out
