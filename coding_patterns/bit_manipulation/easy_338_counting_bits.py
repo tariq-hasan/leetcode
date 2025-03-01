7405f7421dcaf01e568fc4eec20e15108d433e4f
tariq-hasan <mmtariquehsn@gmail.com>
2025-02-28 19:16:36 -0500

    Updated solutions for counting bits

--- coding_patterns/bit_manipulation/easy_338_counting_bits.py
+++ coding_patterns/bit_manipulation/easy_338_counting_bits.py
@@ -6,16 +6,15 @@ class Solution:
         The time complexity is O(n).
         The space complexity is O(1).
         """
-        out = [0]
+        out = [0] * (n + 1)
         j = 1
         while j < n + 1:
             i = 0
             while i < j and i + j <= n:
-                out.append(out[i] + 1)
+                out[i + j] = out[i] + 1
                 i = i + 1
             j = j << 1
         return out
-
 
 
 class Solution:
@@ -25,9 +24,9 @@ class Solution:
         The space complexity is O(1).
         """
         out = [0] * (n + 1)
-        for x in range(1, n + 1):
+        for i in range(1, n + 1):
             # x // 2 is x >> 1 and x % 2 is x & 1
-            out[x] = out[x >> 1] + (x & 1) 
+            out[i] = out[i >> 1] + (i & 1) 
         return out
 
 
