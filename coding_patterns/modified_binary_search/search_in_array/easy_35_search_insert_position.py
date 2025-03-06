166f3613abf68190984a6ee0941126ff95f17b57
tariq-hasan <mmtariquehsn@gmail.com>
2025-03-05 23:39:32 -0500

    Update solution to search insert position

--- coding_patterns/modified_binary_search/search_in_array/easy_35_search_insert_position.py
+++ coding_patterns/modified_binary_search/search_in_array/easy_35_search_insert_position.py
@@ -1,18 +1,19 @@
 from typing import List
 
+
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         """
         The time complexity is O(log n).
         The space complexity is O(1).
         """
-        left, right = 0, len(nums) - 1
-        while left <= right:
-            mid = (left + right) // 2
+        i, j = 0, len(nums) - 1
+        while i <= j:
+            mid = i + ((j - i) // 2)
             if nums[mid] < target:
-                left = mid + 1
+                i = mid + 1
             elif nums[mid] > target:
-                right = mid - 1
+                j = mid - 1
             else:
                 return mid
-        return left
+        return i
