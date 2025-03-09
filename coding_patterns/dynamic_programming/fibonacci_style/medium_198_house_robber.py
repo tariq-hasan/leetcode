349e35b0edc1069dd52d81faf45766212abe68d9
tariq-hasan <mmtariquehsn@gmail.com>
2025-03-08 21:47:50 -0500

    Add solution to house robber problem

--- coding_patterns/dynamic_programming/fibonacci_style/medium_198_house_robber.py
+++ coding_patterns/dynamic_programming/fibonacci_style/medium_198_house_robber.py
@@ -7,6 +7,21 @@ class Solution:
         The time complexity is O(n).
         The space complexity is O(1).
         """
+        if len(nums) == 1:
+            return nums[0]
+
+        i = j = 0
+        for num in nums:
+            i, j = j, max(num + i, j)
+        return j
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        """
+        The time complexity is O(n).
+        The space complexity is O(1).
+        """
         i, j = nums[-1], 0
         for k in range(len(nums) - 2, -1, -1):
             i, j = max(i, j + nums[k]), i
