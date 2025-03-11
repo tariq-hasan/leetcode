acb3f66eaa594c095f94c1ffdadf29928f747c67
tariq-hasan <mmtariquehsn@gmail.com>
2025-03-11 02:25:08 -0400

    Add solution for climbing stairs

---
+++ coding_patterns/dynamic_programming/fibonacci_style/easy_70_climbing_stairs.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        The time complexity is O(n).
+        The space complexity is O(1).
+        """
+        if n == 1:
+            return 1
+        i, j = 1, 2
+        for _ in range(n - 2):
+            i, j = j, i + j
+        return j
