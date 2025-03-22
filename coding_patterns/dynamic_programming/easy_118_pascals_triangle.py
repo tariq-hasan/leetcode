c77b683eaed84e713ccfd2c7f1b8f48820981663
tariq-hasan <mmtariquehsn@gmail.com>
2025-03-22 12:12:07 -0400

    Update solution for pascal's triangle

--- coding_patterns/dynamic_programming/easy_118_pascals_triangle.py
+++ coding_patterns/dynamic_programming/easy_118_pascals_triangle.py
@@ -9,9 +9,9 @@ class Solution:
         """
         out = []
         for i in range(numRows):
-            row = [None for _ in range(i + 1)]
-            row[0], row[-1] = 1, 1
+            row = [0] * (i + 1)
+            row[0] = row[-1] = 1
             for j in range(1, len(row) - 1):
-                row[j] = out[i - 1][j - 1] + out[i - 1][j]
+                row[j] = out[-1][j - 1] + out[-1][j]
             out.append(row)
         return out
