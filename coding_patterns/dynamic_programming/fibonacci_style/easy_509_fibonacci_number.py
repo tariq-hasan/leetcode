f8d9b9b2cc00d5e6e968923a605e5017a36f9c2e
tariq-hasan <mmtariquehsn@gmail.com>
2025-03-22 14:53:29 -0400

    Update solution for fibonacci numbers

--- coding_patterns/dynamic_programming/fibonacci_style/easy_509_fibonacci_number.py
+++ coding_patterns/dynamic_programming/fibonacci_style/easy_509_fibonacci_number.py
@@ -4,9 +4,11 @@ class Solution:
         The time complexity is O(n).
         The space complexity is O(1).
         """
-        if n < 2:
-            return n
+        if n == 0:
+            return 0
+
         i, j = 0, 1
         for _ in range(n - 1):
             i, j = j, i + j
+
         return j
