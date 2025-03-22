4741a73848ba1e911a5e8c42b57716317b93cebb
tariq-hasan <mmtariquehsn@gmail.com>
2025-03-22 00:26:50 -0400

    Update solution for count ways to build good strings

--- coding_patterns/dynamic_programming/general_1d_dp/linear_recurrence_(fibonacci-style)/medium_2466_count_ways_to_build_good_strings.py
+++ coding_patterns/dynamic_programming/general_1d_dp/linear_recurrence_(fibonacci-style)/medium_2466_count_ways_to_build_good_strings.py
@@ -15,3 +15,26 @@ class Solution:
             dp[i] = dp[i] % mod
 
         return sum(dp[low : high + 1]) % mod
+
+
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        """
+        The time complexity is O(n).
+        The space complexity is O(n).
+        """
+        dp = [1] + [-1] * (high)
+        mod = 10 ** 9 + 7
+
+        def dfs(i):
+            if dp[i] != -1:
+                return dp[i]
+            count = 0
+            if i >= zero:
+                count = count + dfs(i - zero)
+            if i >= one:
+                count = count + dfs(i - one)
+            dp[i] = count % mod
+            return dp[i]
+
+        return sum(dfs(i) for i in range(low, high + 1)) % mod
