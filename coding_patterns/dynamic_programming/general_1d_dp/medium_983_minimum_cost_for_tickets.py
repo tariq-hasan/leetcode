7f8f86c97646aecadc994e081946e824a4362885
tariq-hasan <mmtariquehsn@gmail.com>
2025-03-24 23:06:49 -0400

    Add solution for minimum cost for tickets

--- coding_patterns/dynamic_programming/general_1d_dp/medium_983_minimum_cost_for_tickets.py
+++ coding_patterns/dynamic_programming/general_1d_dp/medium_983_minimum_cost_for_tickets.py
@@ -28,3 +28,24 @@ class Solution:
         is_travel_needed = set(days)
 
         return solve(dp, 0)
+
+
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        """
+        The time complexity is O(n), where n is days[-1].
+        The space complexity is O(n), where n is days[-1].
+        """
+        dp = [0] * (days[-1] + 1)
+
+        i = 0
+        for day in range(1, days[-1] + 1):
+            if day < days[i]:
+                dp[day] = dp[day - 1]
+            else:
+                i = i + 1
+                dp[day] = min(dp[day - 1] + costs[0], \
+                              dp[max(0, day - 7)] + costs[1], \
+                              dp[max(0, day - 30)] + costs[2])
+
+        return dp[-1]
