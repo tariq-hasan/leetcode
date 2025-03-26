5a719dd096e815ede16aa22e9153749251dcde1e
tariq-hasan <mmtariquehsn@gmail.com>
2025-03-25 22:13:42 -0400

    Add solution for maximize sum of array after k negations

---
+++ coding_patterns/greedy_techniques/classic_greedy_problems/easy_1005_maximize_sum_of_array_after_k_negations.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
+        """
+        The time complexity is O(n log n).
+        The space complexity is O(1).
+        """
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] < 0 and k > 0:
+                nums[i] = -nums[i]
+                k = k - 1
+        nums = sorted(nums)
+        if k > 0 and k % 2 != 0:
+            nums[0] = - nums[0]
+        return sum(nums)
