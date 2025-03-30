527815739c720e960679ba48a7ba2ea525b6f860
tariq-hasan <mmtariquehsn@gmail.com>
2025-03-30 14:25:10 -0400

    Add solution for flipping an image

--- coding_patterns/bit_manipulation/easy_832_flipping_an_image.py
+++ coding_patterns/bit_manipulation/easy_832_flipping_an_image.py
@@ -10,3 +10,17 @@ class Solution:
             for i in range((len(row) + 1) // 2):
                 row[i], row[~i] = row[~i] ^ 1, row[i] ^ 1
         return image
+
+
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        """
+        The time complexity is O(n).
+        The space complexity is O(1).
+        """
+        for row in image:
+            i, j = 0, len(row) - 1
+            while i <= j:
+                row[i], row[j] = row[j] ^ 1, row[i] ^ 1
+                i, j = i + 1, j - 1
+        return image
