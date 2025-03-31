62f757007cbc1daf84305e38d4cc9d21f23bf6c2
tariq-hasan <mmtariquehsn@gmail.com>
2025-03-31 19:54:45 -0400

    Add solution for single number ii

---
+++ coding_patterns/bit_manipulation/xor_applications/medium_137_single_number_ii.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        """
+        The time complexity is O(n).
+        The space complexity is O(1).
+        """
+        loner = 0
+
+        for shift in range(32):
+            bit_sum = 0
+            for num in nums:
+                bit_sum += (num >> shift) & 1
+            loner_bit = bit_sum % 3
+            loner = loner | (loner_bit << shift)
+
+        if loner >= (1 << 31):
+            loner = loner - (1 << 32)
+
+        return loner
