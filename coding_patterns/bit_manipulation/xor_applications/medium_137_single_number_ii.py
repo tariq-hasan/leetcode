150c9693e5df2a3d7d3f037c4a332fdf9a87b98f
tariq-hasan <mmtariquehsn@gmail.com>
2025-03-31 20:52:37 -0400

    Add second solution for single number ii

--- coding_patterns/bit_manipulation/xor_applications/medium_137_single_number_ii.py
+++ coding_patterns/bit_manipulation/xor_applications/medium_137_single_number_ii.py
@@ -20,3 +20,18 @@ class Solution:
             loner = loner - (1 << 32)
 
         return loner
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        """
+        The time complexity is O(n).
+        The space complexity is O(1).
+        """
+        seen_once = seen_twice = 0
+
+        for num in nums:
+            seen_once = (seen_once ^ num) & (~seen_twice)
+            seen_twice = (seen_twice ^ num) & (~seen_once)
+
+        return seen_once
