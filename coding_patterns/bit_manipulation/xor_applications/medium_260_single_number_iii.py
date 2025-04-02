d1c9256870ebda5aedeb68d2df11d7ce9f0c893b
tariq-hasan <mmtariquehsn@gmail.com>
2025-04-02 01:52:59 -0400

    Add solution for single number ii

---
+++ coding_patterns/bit_manipulation/xor_applications/medium_260_single_number_iii.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        """
+        The time complexity is O(n).
+        The space complexity is O(1).
+        """
+        bitmask = 0
+        for num in nums:
+            bitmask ^= num
+
+        diff = bitmask & (-bitmask)
+
+        x = 0
+        for num in nums:
+            if num & diff:
+                x ^= num
+
+        return [x, bitmask^x]
