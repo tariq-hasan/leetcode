38a3cd2c266cce25828a59b0f62f6a7b9cc7d863
tariq-hasan <mmtariquehsn@gmail.com>
2025-04-05 00:56:53 -0400

    Add solution for same tree

--- coding_patterns/trees/basics/easy_100_same_tree.py
+++ coding_patterns/trees/basics/easy_100_same_tree.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import Optional
 
 
@@ -25,3 +26,38 @@ class Solution:
             return False
 
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        """
+        The time complexity is O(n).
+        The space complexity is O(n).
+        """
+        def check(p: TreeNode, q: TreeNode) -> bool:
+            if not p and not q:
+                return True
+
+            if not p or not q:
+                return False
+
+            if p.val != q.val:
+                return False
+
+            return True
+
+        deq = deque(
+            [
+                (p, q),
+            ]
+        )
+        while deq:
+            p, q = deq.popleft()
+            if not check(p, q):
+                return False
+
+            if p:
+                deq.append((p.left, q.left))
+                deq.append((p.right, q.right))
+
+        return True
