0119c06b69be19b60c77462d750b3e49bf72edd1
tariq-hasan <mmtariquehsn@gmail.com>
2025-04-12 17:29:55 -0400

    Update the solution to combination sum

--- coding_patterns/backtracking/subset_and_combination_patterns/medium_39_combination_sum.py
+++ coding_patterns/backtracking/subset_and_combination_patterns/medium_39_combination_sum.py
@@ -1,28 +1,30 @@
 from typing import List
 
-
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-
+        """
+        The time complexity is O(n^(T/M)).
+        The space complexity is O(T/M).
+        Here, N is the number of candidates,
+        T is the target value,
+        and M is the minimum value in candidates.
+        """
         results = []
 
-        def backtrack(remain, comb, start):
-            if remain == 0:
-                # make a deep copy of the current combination
-                results.append(list(comb))
+        def backtrack(remaining: int, combination: List[int], start: int):
+            if remaining == 0:
+                # Found a valid combination
+                results.append(combination[:])
                 return
-            elif remain < 0:
-                # exceed the scope, stop exploration.
+            if remaining < 0:
+                # Exceeded the target, backtrack
                 return
 
             for i in range(start, len(candidates)):
-                # add the number into the combination
-                comb.append(candidates[i])
-                # give the current number another chance, rather than moving on
-                backtrack(remain - candidates[i], comb, i)
-                # backtrack, remove the number from the combination
-                comb.pop()
+                combination.append(candidates[i])
+                # Allow the same number to be used again by passing i (not i+1)
+                backtrack(remaining - candidates[i], combination, i)
+                combination.pop()  # Undo the last choice
 
         backtrack(target, [], 0)
-
         return results
