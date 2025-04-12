703e208f1378fed2195ff2c52370641d433b4c3c
tariq-hasan <mmtariquehsn@gmail.com>
2025-04-12 15:45:27 -0400

    Update solution for letter combinations of a phone number

--- coding_patterns/backtracking/subset_and_combination_patterns/medium_17_letter_combinations_of_a_phone_number.py
+++ coding_patterns/backtracking/subset_and_combination_patterns/medium_17_letter_combinations_of_a_phone_number.py
@@ -43,25 +43,3 @@ class Solution:
         combinations = []
         backtrack(0, [])
         return combinations
-
-
-class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
-        """
-        The time complexity is O(n * 4^n), where 4 is the maximum value length in letters.
-        The space complexity is O(n).
-        """
-
-        if n == 0:
-            return ['']
-
-        combinations = []
-        for left_count in range(n):
-            for left_string in self.generateParenthesis(left_count):
-                for right_string in self.generateParenthesis(n - 1 - left_count):
-                    combinations.append('(' + left_string + ')' + right_string)
-
-        return combinations
-
-
-
