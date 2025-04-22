85e302e0c67c832e7fa0809d2528101496b1f8f9
tariq-hasan <mmtariquehsn@gmail.com>
2025-04-21 21:56:37 -0400

    Add iterative DFS solution to flood fill

--- coding_patterns/graphs/graph_traversal/easy_733_flood_fill.py
+++ coding_patterns/graphs/graph_traversal/easy_733_flood_fill.py
@@ -38,3 +38,41 @@ class Solution:
 
         dfs(sr, sc)
         return image
+
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        """
+        DFS - Iterative (Using Stack)
+
+        Time Complexity: O(N), where N is the number of pixels in the image.
+        We process each pixel at most once.
+
+        Space Complexity: O(N) for the stack in worst case.
+        The stack might need to store many pixel coordinates in a large connected area.
+        """
+        rows, cols = len(image), len(image[0])
+        original_color = image[sr][sc]
+
+        # Early return if starting pixel is already the target color
+        if original_color == color:
+            return image
+
+        stack = [(sr, sc)]
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]  # Down, Up, Right, Left
+
+        while stack:
+            r, c = stack.pop()
+
+            # Only process uncolored pixels with the original color
+            if image[r][c] == original_color:
+                image[r][c] = color
+
+                # Add valid neighbors to stack
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if (0 <= nr < rows and 0 <= nc < cols and
+                        image[nr][nc] == original_color):
+                        stack.append((nr, nc))
+
+        return image
