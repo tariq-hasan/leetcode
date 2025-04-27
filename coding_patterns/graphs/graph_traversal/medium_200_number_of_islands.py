b880266b0cf5b69e7e38a1839e626b44c2b2e38c
tariq-hasan <mmtariquehsn@gmail.com>
2025-04-27 14:07:36 -0400

    Add iterative DFS solution for number of islands

--- coding_patterns/graphs/graph_traversal/medium_200_number_of_islands.py
+++ coding_patterns/graphs/graph_traversal/medium_200_number_of_islands.py
@@ -34,3 +34,37 @@ class Solution:
                     dfs(r, c)     # Mark all connected land cells
 
         return islands
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        """
+        Iterative DFS Solution for Number of Islands
+
+        Time Complexity: O(M*N) where M is number of rows and N is number of columns
+        Space Complexity: O(M*N) for the stack in worst case
+        """
+        rows, cols = len(grid), len(grid[0])
+        islands = 0
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]  # Down, Up, Right, Left
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1':
+                    islands += 1  # Found a new island
+
+                    # Start DFS from this land cell
+                    stack = [(r, c)]
+                    grid[r][c] = '0'  # Mark as visited immediately
+
+                    while stack:
+                        cur_r, cur_c = stack.pop()
+
+                        # Check all adjacent cells
+                        for dr, dc in directions:
+                            nr, nc = cur_r + dr, cur_c + dc
+                            if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == '1':
+                                stack.append((nr, nc))
+                                grid[nr][nc] = '0'  # Mark as visited when adding to stack
+
+        return islands
